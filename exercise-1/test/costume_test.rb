d7781c0e3e74eaafffa9c3ee9d285a2f60177063
Deborah Hamel <deborahleehamel@gmail.com>
2015-12-29 12:41:40 -0700

    Third object Costume created methods and all tests pass.

--- exercise-1/test/costume_test.rb
+++ exercise-1/test/costume_test.rb
@@ -5,13 +5,11 @@ require_relative '../lib/costume'
 
 class CostumeTest < Minitest::Test
   def test_costume_style
-    skip
     costume = Costume.new("Batman")
     assert_equal "Batman", costume.style
   end
 
   def test_other_style_of_costume
-    skip
     costume = Costume.new("Princess")
     assert_equal "Princess", costume.style
   end
